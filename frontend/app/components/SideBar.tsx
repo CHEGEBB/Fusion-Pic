12ecc91a73cd7ebab4ec6399174e62678a1807dc
CHEGEBB <chegephil24@gmail.com>
2025-02-25 14:22:25 +0300

    Refactor SideBar component and add logo image

--- frontend/app/components/SideBar.tsx
+++ frontend/app/components/SideBar.tsx
@@ -2,12 +2,16 @@ import { FolderDown, HistoryIcon, LayoutDashboard, Sparkles } from 'lucide-react
 import Link from 'next/link'
 import React from 'react'
 import "../sass/Sidebar.scss"
+import Image from 'next/image'
+import Logo from '../assets/logo2.png'
 
 function SideBar() {
   return (
-    <div className='container'>
-        <div className="logo"></div>
-        <div className="flex flex-col justify-between bg-gray-800 border-gray-600 border-r-[1px] h-[100vh] bo nav">
+    <div className='p-2 container'>
+        <div className="bg-slate-400 p-2 rounded-md w-8 h-8 logo">
+            <Image src={Logo} alt='logo' width={24} height={24} color='#fff'/>
+        </div>
+        <div className="flex flex-col justify-center bg-gray-800 border-gray-600 border-r-[1px] h-[100vh] bo nav">
             <div className="nav-element">
                 <Sparkles className="icon-sparkles" width={20} height={20} />
             <Link href='Generate' className='nav-item' />
