47fff61bf795a597553bf719bbe5e0c81acef67c
CHEGEBB <chegephil24@gmail.com>
2025-02-26 08:36:01 +0300

    added styles for skeleton

--- frontend/app/components/Skeleton.tsx
+++ frontend/app/components/Skeleton.tsx
@@ -4,9 +4,7 @@ const Skeleton = () => {
   return (
     <div className="animate-pulse flex-1  flex-col items-center space-y-4">
       {/* Skeleton Image */}
-      <div className="lg:w-[600px] h-[400px] bg-gray-600 rounded-lg"></div>
-      {/* Skeleton Button */}
-      <div className="w-[150px] h-10 bg-gray-700 rounded-md"></div>
+      <div className="lg:w-[600px] lg:h-[500px] bg-gray-600 rounded-lg border-4 border-gray-800"></div>
     </div>
   );
 };
